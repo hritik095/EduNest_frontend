0709e6345654efdea27c96a5ee682baf604be223
hritik095 <hritik22095@gmail.com>
2024-11-02 17:49:46 +0530

    adding frontend

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Header from './components/header/Header';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Verify from './pages/auth/Verify';
+import Footer from './components/footer/Footer';
+import About from './pages/about/About';
+import Account from './pages/account/Account';
+import { UserData } from './context/UserContext';
+import Loading from './components/loading/loading';
+import Courses from './pages/courses/Courses';
+
+function App() {
+  const {isAuth, user, loading}=UserData();
+
+  return (
+    <>
+    {
+      loading?<Loading/>:<BrowserRouter>
+      <Header isAuth ={isAuth}/>
+      <Routes>
+        <Route path="/" element= {<Home/>} />
+        <Route path="/about" element= {<About/>} />
+        <Route path="/courses" element= {<Courses/>} />
+        <Route path="/account" element= {isAuth?<Account user={user}/>:<Login/>} />
+        <Route path="/login" element= {isAuth?<Home/>:<Login/>} />
+        <Route path="/register" element= {isAuth?<Home/>:<Register/>} />
+        <Route path="/verify" element= {isAuth?<Home/>:<Verify/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    }
+    </>
+  )
+}
+
+export default App
